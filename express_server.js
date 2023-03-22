286ff80d1ec9a99e934125c53f9bc346b7610a19
tehi_kim <k.tehi21@gmail.com>
2023-03-22 14:02:44 +0000

    following exercise EJS

--- express_server.js
+++ express_server.js
@@ -7,23 +7,50 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// respond with "hello world" 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// use res.render to load up an ejs view file
+
+// VER1) respond with "hello world" 
 // when a GET request is made to the homepage
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  // VER1) res.send("Hello!");
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+/***** excersise to creat new path(page)
+  app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
 
+  app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  }); //curl -i http://localhost:8080/hello
 
-
-
+  app.get("/set", (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+  });
+  
+  app.get("/fetch", (req, res) => {
+    res.send(`a = ${a}`);
+  });
+ */
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
