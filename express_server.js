68947433c3758275bb63885c573cf0fedd18ac94
tehi_kim <k.tehi21@gmail.com>
2023-03-24 21:40:47 +0000

    seperate data users, urlDatabase and getUserByEmail, urlsForUser function and export/import

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require('morgan');
 const cookieSession = require('cookie-session');
+const {users, urlDatabase, getUserByEmail, urlsForUser} = require('./helpers');
 const bcrypt = require("bcryptjs");
 
 const app = express();
@@ -27,60 +28,12 @@ const generateRandomString = function () {
   return result;
 }
 
-const users = {
-  h0fdkc: {
-    id: 'h0fdkc',
-    email: 'a@a.com',
-    password: '$2a$10$FTlSnoom0j2/CN.hkoke/eDlX20uOFYQmbBLbjBcEAI3JHq2b4oNa'
-  },
-  abgjzj: {
-    id: 'abgjzj',
-    email: 'b@b.com',
-    password: '$2a$10$m/TNVclJzLrkI9o58Ao.f.Wy5oQdzj68VjHMQaRVJeP.yv6P8RZ/G'
-  }
-};
 
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "h0fdkc"
-  },
-  "9sm5xK": {
-    longURL: "http://www.google.com",
-    userID: "h0fdkc"
-  },
-  "s8df12": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "abgjzj"
-  },
-  "12gods": {
-    longURL: "http://www.google.com",
-    userID: "abgjzj"
-  }
-};
 
 let user = {};
 let templateVars = {};
 
-// returns the URLs where the userID is equal to the id of the currently logged-in user.
-const getUserByEmail = function(email) {
-  const values = Object.values(users);
 
-  for (const user of values) {
-    if (user.email === email) {
-      return user;
-    }
-  }
-}
-const urlsForUser = function(id) {
-  const userURL = {};
-  for (let shortId in urlDatabase) {
-    if(urlDatabase[shortId].userID === id) {
-      userURL[shortId] = urlDatabase[shortId]
-    }
-  }
-  return userURL;
-}
 
 // VER1) respond with "hello world" 
 // when a GET request is made to the homepage
---
+++ helpers.js
@@ -0,0 +1,55 @@
+const users = {
+  h0fdkc: {
+    id: 'h0fdkc',
+    email: 'a@a.com',
+    password: '$2a$10$FTlSnoom0j2/CN.hkoke/eDlX20uOFYQmbBLbjBcEAI3JHq2b4oNa'
+  },
+  abgjzj: {
+    id: 'abgjzj',
+    email: 'b@b.com',
+    password: '$2a$10$m/TNVclJzLrkI9o58Ao.f.Wy5oQdzj68VjHMQaRVJeP.yv6P8RZ/G'
+  }
+};
+
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "h0fdkc"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "h0fdkc"
+  },
+  "s8df12": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "abgjzj"
+  },
+  "12gods": {
+    longURL: "http://www.google.com",
+    userID: "abgjzj"
+  }
+};
+
+
+// return matched user info 
+const getUserByEmail = function(email) {
+  const values = Object.values(users);
+  
+  for (const user of values) {
+    if (user.email === email) {
+      return user;
+    }
+  }
+}
+// returns the URLs where the userID is equal to the id of the currently logged-in user.
+const urlsForUser = function(id) {
+  const userURL = {};
+  for (let shortId in urlDatabase) {
+    if(urlDatabase[shortId].userID === id) {
+      userURL[shortId] = urlDatabase[shortId]
+    }
+  }
+  return userURL;
+}
+
+module.exports = {users, urlDatabase, getUserByEmail, urlsForUser};
